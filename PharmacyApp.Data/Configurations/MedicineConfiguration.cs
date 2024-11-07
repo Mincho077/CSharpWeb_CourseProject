4e2221f5aebc65c84d4ec75972d2fff9f2ead633
Mincho077 <MinchoNachev@proton.me>
2024-11-07 06:58:34 +0200

    Comment Medicine seed

--- PharmacyApp.Data/Configurations/MedicineConfiguration.cs
+++ PharmacyApp.Data/Configurations/MedicineConfiguration.cs
@@ -26,137 +26,137 @@
                   .Property(m => m.Price)
                   .HasPrecision(18,2);
 
-            builder.HasData(GenerateMedicines());
+            //builder.HasData(GenerateMedicines());
        
         }
 
-        private Medicine[] GenerateMedicines()
-        {
-            ICollection<Medicine> medicines= new HashSet<Medicine>();
+        //private Medicine[] GenerateMedicines()
+        //{
+        //    ICollection<Medicine> medicines = new HashSet<Medicine>();
 
-            Medicine medicine;
+        //    Medicine medicine;
 
-            medicine = new Medicine()
-            {
-                Name = "Cefzil",
-                ImageUrl = "https://static.framar.bg/thumbs/6/products/cefzil-petstotin-mg.jpg",
-                ActiveIngredient = "cefprozil",
-                ActiveIngredientQuantity = "500 mg",
-                Quantity = 10,
-                MedicineFormId = 2,
-                MedicineTypeId = 1,
-                QuantityInABox = "10 tb",
-                Price = 24.62m,
-                ExpiryDate = DateTime.ParseExact("31-10-2028", MedicineDateFormat, CultureInfo.InvariantCulture),
-                ManufacturerId = 1,
+        //    medicine = new Medicine()
+        //    {
+        //        Name = "Cefzil",
+        //        ImageUrl = "https://static.framar.bg/thumbs/6/products/cefzil-petstotin-mg.jpg",
+        //        ActiveIngredient = "cefprozil",
+        //        ActiveIngredientQuantity = "500 mg",
+        //        Quantity = 10,
+        //        MedicineFormId = 2,
+        //        MedicineTypeId = 1,
+        //        QuantityInABox = "10 tb",
+        //        Price = 24.62m,
+        //        ExpiryDate = DateTime.ParseExact("31-10-2028", MedicineDateFormat, CultureInfo.InvariantCulture),
+        //        ManufacturerId = 1,
 
-            };
+        //    };
 
-            medicines.Add(medicine);
+        //    medicines.Add(medicine);
 
-            medicine = new Medicine()
-            {
-                Name = "Ospamox",
-                ImageUrl = "https://static.framar.bg/thumbs/6/products/ospamox-1000.jpg",
-                ActiveIngredient = "amoxicillin",
-                ActiveIngredientQuantity = "1000 mg",
-                Quantity = 10,
-                MedicineFormId = 1,
-                MedicineTypeId = 1,
-                QuantityInABox = "14 tb",
-                Price = 5.87m,
-                ExpiryDate = DateTime.ParseExact("30-05-2027", MedicineDateFormat, CultureInfo.InvariantCulture),
-                ManufacturerId = 1,
-            };
+        //    medicine = new Medicine()
+        //    {
+        //        Name = "Ospamox",
+        //        ImageUrl = "https://static.framar.bg/thumbs/6/products/ospamox-1000.jpg",
+        //        ActiveIngredient = "amoxicillin",
+        //        ActiveIngredientQuantity = "1000 mg",
+        //        Quantity = 10,
+        //        MedicineFormId = 1,
+        //        MedicineTypeId = 1,
+        //        QuantityInABox = "14 tb",
+        //        Price = 5.87m,
+        //        ExpiryDate = DateTime.ParseExact("30-05-2027", MedicineDateFormat, CultureInfo.InvariantCulture),
+        //        ManufacturerId = 1,
+        //    };
 
-            medicines.Add(medicine);
+        //    medicines.Add(medicine);
 
-            medicine = new Medicine()
-            {
-                Name = "Lexotan",
-                ImageUrl = "https://static.framar.bg/thumbs/6/products/leksotan-tabletki-bromazepam-actavis-lexotan.jpg",
-                ActiveIngredient = "bromazepam",
-                ActiveIngredientQuantity = "10 mg",
-                Quantity = 10,
-                MedicineFormId = 1,
-                MedicineTypeId = 3,
-                QuantityInABox = "30 tb",
-                Price = 5.87m,
-                ExpiryDate = DateTime.ParseExact("30-09-2026", MedicineDateFormat, CultureInfo.InvariantCulture),
-                ManufacturerId = 3,
-            };
+        //    medicine = new Medicine()
+        //    {
+        //        Name = "Lexotan",
+        //        ImageUrl = "https://static.framar.bg/thumbs/6/products/leksotan-tabletki-bromazepam-actavis-lexotan.jpg",
+        //        ActiveIngredient = "bromazepam",
+        //        ActiveIngredientQuantity = "10 mg",
+        //        Quantity = 10,
+        //        MedicineFormId = 1,
+        //        MedicineTypeId = 3,
+        //        QuantityInABox = "30 tb",
+        //        Price = 5.87m,
+        //        ExpiryDate = DateTime.ParseExact("30-09-2026", MedicineDateFormat, CultureInfo.InvariantCulture),
+        //        ManufacturerId = 3,
+        //    };
 
-            medicines.Add(medicine);
+        //    medicines.Add(medicine);
 
-            medicine = new Medicine()
-            {
-                Name = "Diazepam",
-                ImageUrl = "https://static.framar.bg/thumbs/6/products/diazepam-20tablets-5mg.jpg",
-                ActiveIngredient = "diazepam",
-                ActiveIngredientQuantity = "3 mg",
-                Quantity = 10,
-                MedicineFormId = 1,
-                MedicineTypeId = 3,
-                QuantityInABox = "20 tb",
-                Price = 5.87m,
-                ExpiryDate = DateTime.ParseExact("28-11-2025", MedicineDateFormat, CultureInfo.InvariantCulture),
-                ManufacturerId = 3,
-            };
+        //    medicine = new Medicine()
+        //    {
+        //        Name = "Diazepam",
+        //        ImageUrl = "https://static.framar.bg/thumbs/6/products/diazepam-20tablets-5mg.jpg",
+        //        ActiveIngredient = "diazepam",
+        //        ActiveIngredientQuantity = "3 mg",
+        //        Quantity = 10,
+        //        MedicineFormId = 1,
+        //        MedicineTypeId = 3,
+        //        QuantityInABox = "20 tb",
+        //        Price = 5.87m,
+        //        ExpiryDate = DateTime.ParseExact("28-11-2025", MedicineDateFormat, CultureInfo.InvariantCulture),
+        //        ManufacturerId = 3,
+        //    };
 
-            medicines.Add(medicine);
+        //    medicines.Add(medicine);
 
-            medicine = new Medicine()
-            {
-                Name = "Moxogamma",
-                ImageUrl = "https://static.framar.bg/thumbs/6/products/moxogamma-4.jpg",
-                ActiveIngredient = "moxonidine",
-                ActiveIngredientQuantity = "0.4 mg",
-                Quantity = 10,
-                MedicineFormId = 1,
-                MedicineTypeId = 2,
-                QuantityInABox = "30 tb",
-                Price =14.96m,
-                ExpiryDate = DateTime.ParseExact("31-03-2026", MedicineDateFormat, CultureInfo.InvariantCulture),
-                ManufacturerId = 4,
-            };
+        //    medicine = new Medicine()
+        //    {
+        //        Name = "Moxogamma",
+        //        ImageUrl = "https://static.framar.bg/thumbs/6/products/moxogamma-4.jpg",
+        //        ActiveIngredient = "moxonidine",
+        //        ActiveIngredientQuantity = "0.4 mg",
+        //        Quantity = 10,
+        //        MedicineFormId = 1,
+        //        MedicineTypeId = 2,
+        //        QuantityInABox = "30 tb",
+        //        Price = 14.96m,
+        //        ExpiryDate = DateTime.ParseExact("31-03-2026", MedicineDateFormat, CultureInfo.InvariantCulture),
+        //        ManufacturerId = 4,
+        //    };
 
-            medicines.Add(medicine);
+        //    medicines.Add(medicine);
 
-            medicine = new Medicine()
-            {
-                Name = "Bisogamma",
-                ImageUrl = "https://sopharmacy.bg/media/sys_master/h39/h8c/8930734538782.jpg",
-                ActiveIngredient = "bisoprolol fumarate",
-                ActiveIngredientQuantity = "5 mg",
-                Quantity = 10,
-                MedicineFormId = 1,
-                MedicineTypeId = 3,
-                QuantityInABox = "30 tb",
-                Price = 5.50m,
-                ExpiryDate = DateTime.ParseExact("31-08-2027", MedicineDateFormat, CultureInfo.InvariantCulture),
-                ManufacturerId = 4,
-            };
+        //    medicine = new Medicine()
+        //    {
+        //        Name = "Bisogamma",
+        //        ImageUrl = "https://sopharmacy.bg/media/sys_master/h39/h8c/8930734538782.jpg",
+        //        ActiveIngredient = "bisoprolol fumarate",
+        //        ActiveIngredientQuantity = "5 mg",
+        //        Quantity = 10,
+        //        MedicineFormId = 1,
+        //        MedicineTypeId = 3,
+        //        QuantityInABox = "30 tb",
+        //        Price = 5.50m,
+        //        ExpiryDate = DateTime.ParseExact("31-08-2027", MedicineDateFormat, CultureInfo.InvariantCulture),
+        //        ManufacturerId = 4,
+        //    };
 
-            medicines.Add(medicine);
+        //    medicines.Add(medicine);
 
-            medicine = new Medicine()
-            {
-                Name = "",
-                ImageUrl = "https://static.framar.bg/thumbs/6/products/flavamed-za-deca-berlin-chemie.jpg",
-                ActiveIngredient = "ambroxol hydrochloride",
-                ActiveIngredientQuantity = "15 mg/5 ml",
-                Quantity = 10,
-                MedicineFormId = 3,
-                MedicineTypeId = 4,
-                QuantityInABox = "100 ml",
-                Price = 7.20m,
-                ExpiryDate = DateTime.ParseExact("31-07-2025", MedicineDateFormat, CultureInfo.InvariantCulture),
-                ManufacturerId = 5,
-            };
+        //    medicine = new Medicine()
+        //    {
+        //        Name = "",
+        //        ImageUrl = "https://static.framar.bg/thumbs/6/products/flavamed-za-deca-berlin-chemie.jpg",
+        //        ActiveIngredient = "ambroxol hydrochloride",
+        //        ActiveIngredientQuantity = "15 mg/5 ml",
+        //        Quantity = 10,
+        //        MedicineFormId = 3,
+        //        MedicineTypeId = 4,
+        //        QuantityInABox = "100 ml",
+        //        Price = 7.20m,
+        //        ExpiryDate = DateTime.ParseExact("31-07-2025", MedicineDateFormat, CultureInfo.InvariantCulture),
+        //        ManufacturerId = 5,
+        //    };
 
-            medicines.Add(medicine);
+        //    medicines.Add(medicine);
 
-            return medicines.ToArray();
-        }
+        //    return medicines.ToArray();
+        //}
     }
 }
