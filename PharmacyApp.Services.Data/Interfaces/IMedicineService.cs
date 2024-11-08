1506fdc4d4b5f211800d4d44ba559262a896a2f8
Mincho077 <MinchoNachev@proton.me>
2024-11-08 08:54:14 +0200

    Creating and implementing IMedicineService  interface

--- PharmacyApp.Services.Data/Interfaces/IMedicineService.cs
+++ PharmacyApp.Services.Data/Interfaces/IMedicineService.cs
@@ -1,7 +1,9 @@
-﻿namespace PharmacyApp.Services.Data.Interfaces
+﻿using PharmacyApp.Web.ViewModels.Home;
+
+namespace PharmacyApp.Services.Data.Interfaces
 {
     public interface IMedicineService
     {
-
+        Task<IEnumerable<IndexViewModel>> LastThreeOTCMedicinesAsync();
     }
 }
---
+++ PharmacyApp.Services.Data/MedicineService.cs
@@ -0,0 +1,13 @@
+﻿using PharmacyApp.Services.Data.Interfaces;
+using PharmacyApp.Web.ViewModels.Home;
+
+namespace PharmacyApp.Services.Data
+{
+    internal class MedicineService : IMedicineService
+    {
+        public Task<IEnumerable<IndexViewModel>> LastThreeOTCMedicinesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
