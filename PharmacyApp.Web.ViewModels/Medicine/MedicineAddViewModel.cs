399e072d2161af085ae62ad11f3fdc40670285bb
Mincho077 <MinchoNachev@proton.me>
2024-11-24 10:02:05 +0200

    Created View Models for and Medicine add method

--- PharmacyApp.Web.ViewModels/Medicine/MedicineAddViewModel.cs
+++ PharmacyApp.Web.ViewModels/Medicine/MedicineAddViewModel.cs
@@ -1,75 +1,66 @@
 ﻿
 namespace PharmacyApp.Web.ViewModels.Medicine
 {
-    using System.ComponentModel.DataAnnotations.Schema;
     using System.ComponentModel.DataAnnotations;
-    using static PharmacyApp.Common.EntityValidationConstanst.MedicineConstants
+    using static PharmacyApp.Common.EntityValidationConstanst.Medicine;
     public class MedicineAddViewModel
     {
-        [Key]
-        public Guid Id { get; set; }
 
+        public MedicineAddViewModel()
+        {
+            MedicineForms=new HashSet<MedicineFormViewModel>();
+            MedicineTypes=new HashSet<MedicineTypeViewModel>(); ;
+            Manufacturers = new HashSet<ManufacturerViewModel>();
+
+        }
         [Required]
+        [MinLength(MedicineNameMinLength)]
         [MaxLength(MedicineNameMaxLength)]
         public string Name { get; set; } = null!;
 
-        [Required]
+        [Required]      
         [MaxLength(MedicineImageUrlMaxLength)]
         public string ImageUrl { get; set; } = null!;
 
         [Required]
+        [MinLength(MedicineActiveIngredientMinLength)]
         [MaxLength(MedicineActiveIngredientMaxLength)]
         public string ActiveIngredient { get; set; } = null!;
 
         [Required]
+        [MinLength(MedicineActiveIngredientQuantityMinLength)]
         [MaxLength(MedicineActiveIngredientQuantityMaxLength)]
         public string ActiveIngredientQuantity { get; set; } = null!;
 
         [Required]
         public int Quantity { get; set; }
 
-        public DateTime CreatedOn { get; set; }
 
         [Required]
         public int MedicineFormId { get; set; }
 
         [Required]
-        [ForeignKey(nameof(MedicineFormId))]
-        public MedicineForm MedicineForm { get; set; } = null!;
-
-        [Required]
+        [MinLength(MedicineQuantityInABoxMinLength)]
         [MaxLength(MedicineQuantityInABoxMaxLength)]
         public string QuantityInABox { get; set; } = null!;
 
         [Required]
+        [Range(typeof(decimal),MedicinePriceMinValue, MedicinePriceMaxValue)]
         public decimal Price { get; set; }
 
         [Required]
-        public DateTime ExpiryDate { get; set; }
+        public string ExpiryDate { get; set; }= null!;
 
         [Required]
         public int MedicineTypeId { get; set; }
 
-        [Required]
-        [ForeignKey(nameof(MedicineTypeId))]
-        public virtual MedicineType MedicineType { get; set; } = null!;
 
         [Required]
         public int ManufacturerId { get; set; }
 
-        [Required]
-        [ForeignKey(nameof(ManufacturerId))]
-        public virtual Manufacturer Manufacturer { get; set; } = null!;
+        public IEnumerable<MedicineFormViewModel> MedicineForms { get; set; }
+        public IEnumerable<MedicineTypeViewModel> MedicineTypes { get; set; }
+        public IEnumerable<ManufacturerViewModel> Manufacturers { get; set; }
 
-        [Required]
-        public Guid PharmacistId { get; set; }
-
-        [Required]
-        [ForeignKey(nameof(PharmacistId))]
-        public virtual Pharmacist Pharmacist { get; set; } = null!;
-
-        public Guid? PatientId { get; set; }
-
-        public virtual ApplicationUser? Patient { get; set; } = null
     }
 }
---
+++ PharmacyApp.Web.ViewModels/Medicine/MedicineFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PharmacyApp.Web.ViewModels.Medicine
+{
+    public class MedicineFormViewModel
+    {
+        public int Id { get;set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
